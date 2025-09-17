a3e9e2f9ed44bd4989a7583ae0006572ffc99147
vinnietito <vincentnjuguna69@gmail.com>
2025-09-17 10:02:01 +0300

    Commits

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
       
-      <img src={assets.logo} className='w-36' alt="" />
+      <img src={assets.logof} className='w-36' alt="" />
     </div>
   )
 }
