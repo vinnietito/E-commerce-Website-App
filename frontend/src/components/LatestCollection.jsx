9f60c12dc96194cd19c209324ff38f6587a38431
vinnietito <vincentnjuguna69@gmail.com>
2025-09-17 13:25:35 +0300

    Commits

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,14 +1,15 @@
 import React, { useContext } from 'react'
+import Title from './Title'
+import { ShopContext } from '../context/ShopContext' // <-- don’t forget to import
 
 const LatestCollection = () => {
-
-    const { products } = useContext(ShopContext);
-
-    console.log(products);
+  const { products } = useContext(ShopContext);
 
   return (
-    <div>
-      
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'>
+        <Title text1={'LATEST'} text2={'COLLECTIONS'} />
+      </div>
     </div>
   )
 }
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,11 +1,17 @@
-import { createContext } from "react";
+import React, { createContext } from "react";
+import { products } from "../assets/assets";
 
 
 export const ShopContext = createContext();
 
-const ShopContextProvider = () => {
-    const value = {
+const ShopContextProvider = (props) => {
 
+    const currency = 'Ksh.';
+    const delivery_fee = 350;
+
+
+    const value = {
+        products, currency, delivery_fee
     }
 
     return (
