5a9f0dcb00bf925c19294c2d99ac53b99a6cd397
vinnietito <vincentnjuguna69@gmail.com>
2025-09-18 10:42:13 +0300

    Commits

--- frontend/src/components/NewsletterBox.jsx
+++ frontend/src/components/NewsletterBox.jsx
@@ -8,7 +8,7 @@ const NewsletterBox = () => {
 
   return (
     <div className='text-center'>
-        <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 20% off</p>
+        <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 20% off <br/>Don’t miss out on our exclusive offers and latest collections.</p>
         <p className='text-gray-400 mt-3'>
             Join our mailing list to receive exclusive deals, 
             style inspiration, and updates on our latest arrivals. Be 
