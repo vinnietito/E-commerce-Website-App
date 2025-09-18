ab2c91fe625af27b35626154130f890668686194
vinnietito <vincentnjuguna69@gmail.com>
2025-09-18 10:42:13 +0300

    Commits

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
     <div>
-      
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+        <div>
+            <img src={assets.logof} className='mb-5 w-32' alt="" />
+            <p className='w-full md:w-2/3 text-gray-600'>
+                Your one-stop shop for quality fashion and lifestyle products. We bring you 
+                the latest collections at affordable prices.<br/>
+                Vin Thrift Shop © 2025 – All rights reserved. Crafted with love for our customers worldwide.
+            </p>
+        </div>
+        <div>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy Policy</li>
+            </ul>
+        </div>
+        <div>
+            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>+254792769123</li>
+                <li>vinthriftstore@gmail.com</li>
+
+            </ul>
+        </div>
+      </div>
     </div>
   )
 }
