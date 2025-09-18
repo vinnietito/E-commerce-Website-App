a39992905b7b7faeada8efb89aad00b4b07ef83f
vinnietito <vincentnjuguna69@gmail.com>
2025-09-18 09:53:46 +0300

    Commits

--- frontend/src/components/NewsletterBox.jsx
+++ frontend/src/components/NewsletterBox.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 const NewsletterBox = () => {
+
+    const onSubmitHandler = (event) =>{
+        event.preventDefault();
+    }
+
   return (
     <div className='text-center'>
         <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 20% off</p>
@@ -9,8 +14,9 @@ const NewsletterBox = () => {
             style inspiration, and updates on our latest arrivals. Be 
             the first to know about special offers and seasonal collections!
         </p>
-        <form>
+        <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
             <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Enter your email' required/>
+            <button type='submit' className='bg-black text-white text-xs px-10 py-4'>SUBSCRIBE</button>
         </form>
     </div>
   )
