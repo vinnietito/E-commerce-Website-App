ab3315265fed9247ca54bbc9068ee6d1a0273dcb
vinnietito <vincentnjuguna69@gmail.com>
2025-09-19 20:55:31 +0300

    Commits

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import { products } from "../assets/assets";
 
 
@@ -8,10 +8,13 @@ const ShopContextProvider = (props) => {
 
     const currency = 'Ksh.';
     const delivery_fee = 350;
+    const [search,setSearch] = useState('');
+    const [showSearch,setShowSearch] = useState(true)
 
 
     const value = {
-        products, currency, delivery_fee
+        products, currency, delivery_fee,
+        search,setSearch,showSearch,setShowSearch
     }
 
     return (
