8460567f1c770575e977cb4fdc29135eef3ecedf
vinnietito <vincentnjuguna69@gmail.com>
2025-09-22 20:12:10 +0300

    Commits

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -17,7 +17,6 @@ const Product = () => {
       if (item._id === productId) {
         setProductData(item)
         setImage(item.image[0])
-        console.log(item);
         return null;
       }
     })
