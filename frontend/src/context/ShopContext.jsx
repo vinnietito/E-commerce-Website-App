905146692f221387fba38f7fa48c68df891156e3
vinnietito <vincentnjuguna69@gmail.com>
2025-09-23 10:57:45 +0300

    Commits

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 
 export const ShopContext = createContext();
@@ -12,6 +13,7 @@ const ShopContextProvider = (props) => {
     const [search,setSearch] = useState('');
     const [showSearch,setShowSearch] = useState(false);
     const [cartItems,setCartItems] = useState({});
+    const navigate = useNavigate();
 
     const addToCart = async (itemId,size) => {
 
@@ -84,7 +86,8 @@ const ShopContextProvider = (props) => {
         search,setSearch,showSearch,setShowSearch,
         cartItems,addToCart,
         getCartCount,updateQuantity,
-        getCartAmount
+        getCartAmount,
+        navigate
     }
 
     return (
