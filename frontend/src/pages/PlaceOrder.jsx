b8a20ba59cedbad7082349989170f6aec815a222
vinnietito <vincentnjuguna69@gmail.com>
2025-09-23 11:53:15 +0300

    Commits

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
 import CartTotal from '../components/CartTotal'
+import { assets } from '../assets/assets'
 
 const PlaceOrder = () => {
   return (
@@ -37,8 +38,20 @@ const PlaceOrder = () => {
 
         <div className='mt-12'>
             <Title text1={'PAYMENT'} text2={'METHOD'}/>
+            {/* -------------- Payment Method Selection ---------- */}
             <div className='flex gap-3 flex-col lg:flex-row'>
-
+                <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                  <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+                  <img className='h-5 mx-4' src={assets.stripe_logo} alt="" />
+                </div>
+                <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                  <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+                  <img className='h-5 mx-4' src={assets.razorpay_logo} alt="" />
+                </div>
+                <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                  <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+                  <p className='text-gray-500 text-sm font-medium mx-4'>CASH ON DELIVERY</p>
+                </div>
             </div>
         </div>
 
