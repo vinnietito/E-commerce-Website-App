313720c127efb63890e655ca30293baa3f9eb5e7
vinnietito <vincentnjuguna69@gmail.com>
2025-09-23 10:43:02 +0300

    Commits

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,10 +2,11 @@ import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title';
 import { assets } from '../assets/assets';
+import CartTotal from '../components/CartTotal';
 
 const Cart = () => {
 
-  const { products, currency, cartItems } = useContext(ShopContext);
+  const { products, currency, cartItems,updateQuantity } = useContext(ShopContext);
 
   const [cartData,setCartData] = useState([]);
 
@@ -52,12 +53,18 @@ const Cart = () => {
                     </div>
                   </div>
                 </div>
-                <input className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity} />
+                <input onChange={(e)=> e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id, item.size,Number(e.target.value))} className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity} />
                 <img onClick={()=>updateQuantity(item._id,item.size,0)} className='w-4 mr-4 sm:w-5 cursor-pointer' src={assets.bin_icon} alt="" />
               </div>
             )
           })
         }
+      </div>
+
+      <div className='flex justify-end my-20'>
+          <div className='w-full sm:w- [450px]'>
+            <CartTotal/>
+          </div>
       </div>
 
     </div>
