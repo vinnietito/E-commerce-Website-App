79662420bf63173cdcd0e4f81ff3d047b31fd687
vinnietito <vincentnjuguna69@gmail.com>
2025-09-23 11:14:11 +0300

    Commits

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Title from '../components/Title'
+import CartTotal from '../components/CartTotal'
 
 const PlaceOrder = () => {
   return (
@@ -11,12 +12,38 @@ const PlaceOrder = () => {
             <Title text1={'DELIVERY'} text2={'INFORMATION'}/>
         </div>
         <div className='flex gap-3'>
+            <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='First Name' />
+            <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Last Name' />
+        </div>
+        <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="email" placeholder='Email address' />
+        <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Street' />
+        <div className='flex gap-3'>
+            <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='City' />
+            <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='State' />
+        </div>
+        <div className='flex gap-3'>
+            <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="number" placeholder='Zipcode' />
+            <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Country' />
+        </div>
+        <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="number" placeholder='Phone' />
+      </div>
+      
+      {/* ---------------- Right Side ------------------ */}
+      <div className='mt-8'>
 
+        <div className='mt-8 min-w-80'>
+            <CartTotal/>
+        </div>
+
+        <div className='mt-12'>
+            <Title text1={'PAYMENT'} text2={'METHOD'}/>
+            <div className='flex gap-3 flex-col lg:flex-row'>
+
+            </div>
         </div>
 
       </div>
-      
-      
+
     </div>
   )
 }
