b1182efbe0ed84610c6e2d3d8822cb1d509c6187
vinnietito <vincentnjuguna69@gmail.com>
2025-09-24 12:57:12 +0300

    Commits

---
+++ backend/config/mongodb.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+
+    mongoose.connection.on('connected', () => {
+        console.log('MongoDB connected successfully');
+    });
+
+    await mongoose.connect(`${process.env.MONGODB_URL}/e-commerce`)
+
+}
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
 
 // App Config
 const app = express()
 const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
 
 // Middlewares
 app.use(express.json())
