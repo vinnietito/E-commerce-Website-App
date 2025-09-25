04fccb6f8f1fa64ebf84be11e4bc3560a8ee732c
vinnietito <vincentnjuguna69@gmail.com>
2025-09-25 17:41:59 +0300

    Commits

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
 
 // App Config
 const app = express()
@@ -15,6 +16,8 @@ app.use(express.json())
 app.use(cors())
 
 // API Endpoints
+app.use('/api/user',userRouter)
+
 app.get('/', (req, res) => {
   res.status(200).send("API Working!")
 })
