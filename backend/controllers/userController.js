24081b9b8858d6235c4c82a6240e2714e18f025c
vinnietito <vincentnjuguna69@gmail.com>
2025-09-25 23:37:22 +0300

    Commits

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,15 @@
+import validator from "validator";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
-import { isValidElement } from "react";
+
+
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET)
+}
 
 // Route for user login
 const loginUser = async (req,res) => {
-
-
 
 }
 
@@ -21,11 +26,38 @@ const registerUser = async (req,res) => {
         }
 
         // Validating email format and strong password
-        if
+        if (!validator.isEmail(email)) {
+            return  res.json({success: false, message: "Please enter a valid Email!"});
+        }
+        if (password.length < 8) {
+            return  res.json({success: false, message: "Please enter a strong password!"});
+        }
+
+        // Hashing password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Creating new user
+        const newUser = new userModel({
+            name,
+            email,
+            password: hashedPassword
+            //cartData: { items: [] }
+        })
+
+        const user = await newUser.save();
+
+        console.log("JWT_SECRET from env:", process.env.JWT_SECRET);
+
+
+        const token = createToken(user._id);
+
+        res.json({success: true, token});
 
 
     } catch (error) {
-        
+        console.log(error);
+        res.json({success: false, message:error.message})
     }
 
 }
