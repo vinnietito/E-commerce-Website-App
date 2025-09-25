509efd0683ca2cb8b7e3a1691442fced9e8dc90a
vinnietito <vincentnjuguna69@gmail.com>
2025-09-26 00:05:10 +0300

    Commits

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,7 +10,31 @@ const createToken = (id) => {
 
 // Route for user login
 const loginUser = async (req,res) => {
+    try {
 
+        const { email, password } = req.body;
+
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+            return res.json({success: false, message: "User does not exist!"});
+        }   
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch) {
+
+            const token = createToken(user._id)
+            res.json({success: true, token})
+
+        }
+        else {
+            res.json({success: false, message: "Invalid Credentials!"})
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message:error.message})
+    }
 }
 
 // Route for user Registration
@@ -46,9 +70,7 @@ const registerUser = async (req,res) => {
         })
 
         const user = await newUser.save();
-
-        console.log("JWT_SECRET from env:", process.env.JWT_SECRET);
-
+        
 
         const token = createToken(user._id);
 
