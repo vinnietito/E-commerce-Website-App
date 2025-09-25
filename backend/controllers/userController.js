6a42602ed7b75b804c5ddc1f5f53b66032b52848
vinnietito <vincentnjuguna69@gmail.com>
2025-09-25 17:41:59 +0300

    Commits

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,5 +9,13 @@ const loginUser = async (req,res) => {
 // Route for user Registration
 const registerUser = async (req,res) => {
 
+    res.json({message:"Register API Working"})
 
 }
+
+// Route for admin login
+const adminLogin = async (req,res) => {
+
+}
+
+export { loginUser, registerUser, adminLogin };
