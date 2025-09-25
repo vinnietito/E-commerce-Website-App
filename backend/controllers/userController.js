66ab4d02601e99ea6622e3302733b67e9e2c7400
vinnietito <vincentnjuguna69@gmail.com>
2025-09-25 18:53:51 +0300

    Commits

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,5 @@
+import userModel from "../models/userModel.js";
+import { isValidElement } from "react";
 
 // Route for user login
 const loginUser = async (req,res) => {
@@ -9,7 +11,22 @@ const loginUser = async (req,res) => {
 // Route for user Registration
 const registerUser = async (req,res) => {
 
-    res.json({message:"Register API Working"})
+    try {
+        const { name, email, password } = req.body;
+
+        // Check if user already exists
+        const exists = await userModel.findOne({ email });
+        if (exists) {
+            return res.json({success: false, message: "User already exists!"});
+        }
+
+        // Validating email format and strong password
+        if
+
+
+    } catch (error) {
+        
+    }
 
 }
 
