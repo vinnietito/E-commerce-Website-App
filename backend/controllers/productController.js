ea2b63b3fde9f38f6b989a96ef572a193aba52b4
vinnietito <vincentnjuguna69@gmail.com>
2025-09-26 17:53:25 +0300

    Commits

---
+++ backend/controllers/productController.js
@@ -0,0 +1,25 @@
+
+
+// Function for add product
+const addProduct = async (req,res) => {
+
+}
+
+// Function for list products
+const listProducts = async (req,res) => { 
+
+
+}
+
+// Function for Removing
+const removeProduct = async (req,res) => {
+
+}
+
+
+// Function for single product info
+const singleProduct = async (req,res) => {
+
+}
+
+export { addProduct, listProducts, removeProduct, singleProduct } 
