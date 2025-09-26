2e7868d6400511114d86dbf325f55cd89e422e3d
vinnietito <vincentnjuguna69@gmail.com>
2025-09-26 18:29:55 +0300

    Commits

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,7 +2,25 @@
 
 // Function for add product
 const addProduct = async (req,res) => {
+    try {
+        
+        const { name, price, description, category, subCategory, sizes, bestSeller } = req.body;
 
+        const image1 = req.files.image1[0]
+        const image2 = req.files.image2[0]
+        const image3 = req.files.image3[0]
+        const image4 = req.files.image4[0]
+
+        console.log(name, price, description, category, subCategory, sizes, bestSeller);
+        console.log(image1, image2, image3, image4);
+
+        res.json({})
+        
+
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message:error.message})
+    }
 }
 
 // Function for list products
