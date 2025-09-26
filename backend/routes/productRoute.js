bfe8b6f5392515c57949739bb3ed2fa217049958
vinnietito <vincentnjuguna69@gmail.com>
2025-09-26 18:29:55 +0300

    Commits

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { addProduct, listProducts, removeProduct, singleProduct } from "../controllers/productController.js";
+import upload from "../middleware/multer.js";
 
 const productRouter = express.Router();
 
 // Route for adding a product
-productRouter.post('/add', addProduct);
+productRouter.post('/add',upload.fields([{name:'image1', maxCount:1}, {name:'image2', maxCount:1}, {name:'image3', maxCount:1}, {name:'image4', maxCount:1}]), addProduct);
 
 // Route for removing a product
 productRouter.post('/remove', removeProduct);
