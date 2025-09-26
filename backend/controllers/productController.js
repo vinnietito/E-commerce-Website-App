f60eaa76825fe6602fc49d17993e781c127cf9a6
vinnietito <vincentnjuguna69@gmail.com>
2025-09-26 19:43:59 +0300

    Commits

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
-
+import { v2 as cloudinary } from 'cloudinary';
+import productModel from '../models/productModel.js';
 
 // Function for add product
 const addProduct = async (req,res) => {
@@ -11,10 +12,33 @@ const addProduct = async (req,res) => {
         const image3 = req.files.image3 && req.files.image3[0]
         const image4 = req.files.image4 && req.files.image4[0]
 
-        console.log(name, price, description, category, subCategory, sizes, bestSeller);
-        console.log(image1, image2, image3, image4);
+        const images = [image1, image2, image3, image4].filter((item) => item !== undefined);
+        
+        let imagesUrl = await Promise.all(
+            images.map(async (item) => {
+                let result = await cloudinary.uploader.upload(item.path, {resource_type:'image'} );
+                return result.secure_url
+            })
+        )
 
-        res.json({})
+        const productData = {
+            name,
+            description,
+            category,
+            price: Number(price),
+            subCategory,
+            bestSeller: bestSeller === "true" ? true : false,
+            sizes: JSON.parse(sizes),
+            image: imagesUrl,
+            date: Date.now()
+        }
+
+        console.log(productData);
+
+        const product = new productModel(productData);
+        await product.save();
+
+        res.json({success: true, message: "Product added successfully!"})
         
 
     } catch (error) {
