799fa8d698e9a98601871ec869f00917295ae578
vinnietito <vincentnjuguna69@gmail.com>
2025-09-29 18:09:10 +0300

    Commits

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const List = () => {
+
+    const [list,setList] = useState([])
+
+    const fetchList = async()=>{
+        try {
+            
+            const response = await axios.get(backendURL + '/api/product/list')
+            console.log(response.data);
+
+        } catch (error) {
+            
+        }
+    }
+
+    useEffect(()=>{
+        fetchList()
+    },[])
+
+
   return (
     <div>
       
