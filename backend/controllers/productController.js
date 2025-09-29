8a1a99a44137bafabccf6a8f2cfc4128c7280b87
vinnietito <vincentnjuguna69@gmail.com>
2025-09-29 08:52:31 +0300

    Commits

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -49,7 +49,14 @@ const addProduct = async (req,res) => {
 
 // Function for list products
 const listProducts = async (req,res) => { 
-
+    try {
+        
+        const products = await productModel.find({});
+        res.json({success:true, products})
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message:error.message})
+    }
 
 }
 
