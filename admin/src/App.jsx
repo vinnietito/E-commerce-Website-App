d14c5281a0e507788e0c806810ad58d7df6c5419
vinnietito <vincentnjuguna69@gmail.com>
2025-09-29 13:04:33 +0300

    Commits

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+//import React from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Add from './pages/Add'
+import List from './pages/List'
+import Orders from './pages/Orders'
+import Login from './components/Login'
 
 const App = () => {
+
+  const [token, setToken] = useState('');
+
   return (
     <div className='bg-gray-50 min-h-screen'>
-      <>
-      <Navbar />
-      <hr />
-      <div className='flex w-full'>
-        <Sidebar/>
-            
-      </div>
-      </>
-      
+      {token === ""
+        ? <Login />
+        : <>
+          <Navbar />
+          <hr />
+          <div className='flex w-full'>
+            <Sidebar />
+            <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+              <Routes>
+                <Route path='/add' element={<Add />} />
+                <Route path='/list' element={<List />} />
+                <Route path='/orders' element={<Orders />} />
+              </Routes>
+
+            </div>
+
+          </div>
+        </>
+      }
+
+
     </div>
   )
 }
