a6dc3e3746463e2430cb26918628cb22d02b9f5d
vinnietito <vincentnjuguna69@gmail.com>
2025-09-29 12:16:28 +0300

    Commits

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {assets} from '../assets/assets'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='flex items-center py-2 px-[4%] justify-between shadow-md'>
       <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
       <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
