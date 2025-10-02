90e272e0b97bb0004d3ffa4c9c14cf5b56f0be59
vinnietito <vincentnjuguna69@gmail.com>
2025-10-02 18:49:05 +0300

    Commits

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,8 +1,22 @@
+import userModel from "../models/userModel"
 
 
 
 // add products to user cart
 const addToCart = async (req, res) => {
+
+    try {
+        
+        const { userId, ItemId, size } = req.body
+
+        const userData = await userModel.findById(userId)
+        let cartData = await userData.cartData;
+
+        if
+
+    } catch (error) {
+        
+    }
 
 
 }
--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { addToCart, getCartItems, getUserCart, removeCartItem, updateCart } from '../controllers/cartController.js';
+import authUser from '../middlewares/authMiddleware.js'
 
 const cartRouter = express.Router()
 
-cartRouter.post('/get', getUserCart)
-cartRouter.post('/add', addToCart)
-cartRouter.post('/update', updateCart)
+cartRouter.post('/get', authUser, getUserCart)
+cartRouter.post('/add', authUser, addToCart)
+cartRouter.post('/update', authUser, updateCart)
 
 export default cartRouter
