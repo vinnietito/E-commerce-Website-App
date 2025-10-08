7bf838b2769f4403b17c0965938e15d5c61461c5
vinnietito <vincentnjuguna69@gmail.com>
2025-10-08 13:13:25 +0300

    Commits

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -40,6 +40,17 @@ const ShopContextProvider = (props) => {
             cartData[itemId][size] = 1;
         }
         setCartItems(cartData);
+
+        if (token) {
+            try {
+                
+                await axios.post(backendURL + '/api/cart/add', {itemId,size}, {headers: {token}})
+            } catch (error) {
+                console.log(error)
+                toast.error(error.message)
+            }
+        }
+
     }
 
     const getCartCount = () => {
