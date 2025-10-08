4a4835df2c3c867183895540e9ce69bfb96de20c
vinnietito <vincentnjuguna69@gmail.com>
2025-10-08 18:06:22 +0300

    Commits

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,36 @@
+
+
+// Placing orders using Cash On Delivery(COD) Method
+const placeOrder = async (req, res) => {
+
+}
+
+// Placing orders using Stripe Method
+const placeOrderStripe = async (req, res) => {
+    
+}
+
+// Placing orders using Razor Pay Method
+const placeOrderRazorpay = async (req, res) => {
+    
+}
+
+// Placing orders using Mpesa Method
+const placeOrderMpesa = async (req, res) => {
+    
+}
+
+// All orders Data for Admin Panel
+const allOrders = async (req, res) => {
+    
+}
+
+// User Order Data for Frontend
+const userOrders = async (req, res) => {
+    
+}
+
+// Update Order status
+const updateStatus = async (req, res) => {
+    
+}
