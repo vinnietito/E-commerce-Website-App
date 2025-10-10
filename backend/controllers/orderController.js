91fc5115d0d5fa4ee76cb49f0cba29ba2c93981a
vinnietito <vincentnjuguna69@gmail.com>
2025-10-10 11:15:10 +0300

    Commits

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,7 +1,35 @@
-
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
 
 // Placing orders using Cash On Delivery(COD) Method
 const placeOrder = async (req, res) => {
+
+    try {
+        
+        const { userId, items, amount, address } = req.body;
+
+        const orderData = {
+            userId,
+            items,
+            address,
+            amount,
+            paymentMethod:"COD",
+            payment:false,
+            Date:Date.now()
+        }
+
+        const newOrder = new orderModel(orderData)
+        await newOrder.save()
+
+        await userModel.findByIdAndUpdate(userId,{cartData:{}})
+
+        res.json({success:true, message:"Order Placed"})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+        
+    }
 
 }
 
