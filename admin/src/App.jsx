a0b9119b325e7cbebf4afa4ee0ba195f9adf757c
vinnietito <vincentnjuguna69@gmail.com>
2025-10-14 16:09:19 +0300

    Commits

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -20,31 +20,30 @@ const App = () => {
 }, [token])
 
   return (
+  <BrowserRouter>
     <div className='bg-gray-50 min-h-screen'>
       <ToastContainer />
       {token === ""
-        ? <Login setToken={setToken}/>
+        ? <Login setToken={setToken} />
         : <>
-          <Navbar setToken={setToken} />
-          <hr />
-          <div className='flex w-full'>
-            <Sidebar />
-            <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
-              <Routes>
-                <Route path='/add' element={<Add token={token}/>} />
-                <Route path='/list' element={<List token={token}/>} />
-                <Route path='/orders' element={<Orders token={token}/>} />
-              </Routes>
-
+            <Navbar setToken={setToken} />
+            <hr />
+            <div className='flex w-full'>
+              <Sidebar />
+              <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+                <Routes>
+                  <Route path='/add' element={<Add token={token} />} />
+                  <Route path='/list' element={<List token={token} />} />
+                  <Route path='/orders' element={<Orders token={token} />} />
+                </Routes>
+              </div>
             </div>
-
-          </div>
-        </>
+          </>
       }
-
-
     </div>
-  )
+  </BrowserRouter>
+)
+
 }
 
 export default App
