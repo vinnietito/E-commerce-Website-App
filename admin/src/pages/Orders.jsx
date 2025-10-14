94b4b32529ff10a1a7c7e33a913da32d2379609f
vinnietito <vincentnjuguna69@gmail.com>
2025-10-14 15:12:52 +0300

    Commits

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -4,10 +4,11 @@ import { useState } from 'react'
 import axios from 'axios'
 import { backendURL } from '../App'
 import {toast} from 'react-toastify'
+import { assets } from '../assets/assets'
 
 const Orders = ({token}) => {
 
-  const [Orders,setOrders] = useState([])
+  const [orders, setOrders] = useState([])
 
   const fetchAllOrders = async () => {
 
@@ -36,7 +37,23 @@ const Orders = ({token}) => {
 
   return (
     <div>
-      
+      <h3>Order Page</h3>
+      <div>
+        {
+          orders.map((order, index) => (
+            <div key={index}>
+              <img src={assets.parcel_icon} alt="" />
+              <div>
+                {order.items.map((item,index) => {
+                  if (index === order.items.length) {
+                    return <p key={index}> { item.name } X {item.quantity} <span> {item.size}</span></p>
+                  }
+                })}
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
