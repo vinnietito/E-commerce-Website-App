193a3c63425c52f13525100355d3258d7a1f7df8
vinnietito <vincentnjuguna69@gmail.com>
2025-10-15 12:14:32 +0300

    Commits

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import axios from 'axios'
-import { backendURL } from '../App'
-import {toast} from 'react-toastify'
+import { backendURL, currency } from '../App'
+import { toast } from 'react-toastify'
 import { assets } from '../assets/assets'
 
-const Orders = ({token}) => {
+const Orders = ({ token }) => {
 
   const [orders, setOrders] = useState([])
 
@@ -18,22 +18,22 @@ const Orders = ({token}) => {
 
     try {
 
-      const response = await axios.post(backendURL + '/api/order/list', {}, { headers: { token }})
+      const response = await axios.post(backendURL + '/api/order/list', {}, { headers: { token } })
       if (response.data.success) {
         setOrders(response.data.orders)
       } else {
         toast.error(response.data.message)
       }
-      
+
     } catch (error) {
       toast.error(error.message)
     }
 
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchAllOrders();
-  },[token])
+  }, [token])
 
   return (
     <div>
@@ -43,20 +43,41 @@ const Orders = ({token}) => {
           orders.map((order, index) => (
             <div key={index}>
               <img src={assets.parcel_icon} alt="" />
+
               <div>
-                {order.items.map((item,index) => {
-                  if (index === order.items.length - 1) {
-                    return <p key={index}> { item.name } X {item.quantity} <span> {item.size}</span></p>
-                  } else {
-                    return <p key={index}> { item.name } X {item.quantity} <span> {item.size}</span> ,</p>
-                  }
-                })}
+
+                <div>
+                  {order.items.map((item, index) => {
+                    if (index === order.items.length - 1) {
+                      return <p key={index}> {item.name} X {item.quantity} <span> {item.size}</span></p>
+                    } else {
+                      return <p key={index}> {item.name} X {item.quantity} <span> {item.size}</span> ,</p>
+                    }
+                  })}
+                </div>
+                <p>{order.address.firstName + " " + order.address.lastName}</p>
+                <div>
+                  <p>{order.address.street + " ,"}</p>
+                  <p>{order.address.city + ", " + order.address.state + ", " + order.address.country + ", " + order.address.zipcode}</p>
+                </div>
+                <p>{order.address.phone}</p>
               </div>
-              <p>{order.address.firstName + " " + order.address.lastName}</p>
+
               <div>
-                <p>{order.address.street + " ," }</p>
-                <p>{order.address.city + ", " + order.address.state + ", " + order.address.country + ", " + order.address.zipcode }</p>
+                <p>Items : {order.items.length}</p>
+                <p>Method : {order.paymentMethod}</p>
+                <p>Payment : {order.payment ? 'Done' : 'Pending'}</p>
+                <p>Date : {new Date(order.date).toLocaleDateString()}</p>
               </div>
+              <p>{currency} {order.amount}</p>
+              <select>
+                <option value="Order Placed">Order Placed</option>
+                <option value="Packing">Packing</option>
+                <option value="Shipped">Shipped</option>
+                <option value="Out for delivery">Out for delivery</option>
+                <option value="Delivered">Delivered</option>
+              </select>
+
             </div>
           ))
         }
