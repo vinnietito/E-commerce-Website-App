5bf9a5b8e931d8a622bf9a4330c142ade77430e4
vinnietito <vincentnjuguna69@gmail.com>
2025-10-17 17:14:26 +0300

    Commits

---
+++ backend/utils/mpesa.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+
+// Generate access token
+export const getMpesaAccessToken = async () => {
+  const consumerKey = process.env.MPESA_CONSUMER_KEY;
+  const consumerSecret = process.env.MPESA_CONSUMER_SECRET;
+  const auth = Buffer.from(`${consumerKey}:${consumerSecret}`).toString("base64");
+
+  const response = await axios.get(
+    "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
+    { headers: { Authorization: `Basic ${auth}` } }
+  );
+
+  return response.data.access_token;
+};
