de6750f0ef62df8ed576fdd4c28f37c3bc61cb82
vinnietito <vincentnjuguna69@gmail.com>
2025-10-22 13:01:15 +0300

    Commits

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -21,7 +21,7 @@ const Orders = ({ token }) => {
 
       const response = await axios.post(backendURL + '/api/order/list', {}, { headers: { token } })
       if (response.data.success) {
-        setOrders(response.data.orders)
+        setOrders(response.data.orders.reverse())
       } else {
         toast.error(response.data.message)
       }
